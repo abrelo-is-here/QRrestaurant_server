58a6af55bd9de52801f6b2ab4df7c495666f96df
Abreham Solomon <159897417+abrelo-is-here@users.noreply.github.com>
2026-07-29 15:21:05 +0300

    Change SameSite attribute of qrSession cookie to 'none'

--- controllers/qrController.js
+++ controllers/qrController.js
@@ -63,7 +63,7 @@ export const scanQr = async (req, res) => {
 res.cookie("qrSession", sessionId, {
   httpOnly: false,
   secure: false,
-  sameSite: "lax",
+  sameSite: "none",
   maxAge: 10 * 60 * 1000,
 });
 
